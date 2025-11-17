610b976d55246abbdcfd2638ddb7c22d8da06825
Djerad Abdelheq <93623480+Djerad@users.noreply.github.com>
2025-11-17 03:31:07 +0100

    Update Hive_deatilsPagr.dart

--- lib/Hive_deatilsPagr.dart
+++ lib/Hive_deatilsPagr.dart
@@ -29,7 +29,7 @@ class _HiveDashboardPageState extends State<HiveDashboardPage> {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String? token = prefs.getString("access_token");
 
-      final url = Uri.parse("http://192.168.15.125:8000/api/hives/");
+      final url = Uri.parse("http://ip:8000/api/hives/");
       final response = await http.get(url, headers: {
         "Authorization": "Bearer $token",
         "Content-Type": "application/json",
